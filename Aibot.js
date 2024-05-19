02039527818d23f1b44a11d39e6f1b6142c7df80
周星星✨ <377972932@qq.com>
2024-05-19 10:13:25 +0800

    Add files via upload

---
+++ Aibot.js
@@ -0,0 +1,13 @@
+{
+  "msg" : "ok",
+  "data" : {
+    "vipLabel" : "1",
+    "imgNum" : 0,
+    "id" : "1791865683010625537",
+    "nickname" : "周星星",
+    "chatNum" : 6,
+    "email" : "377972932@qq.com",
+    "vipStatus" : "1"
+  },
+  "code" : 200
+}
